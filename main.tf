23743a4cacafb22e03bd5b22ecb00c5e5d10c113
eeranagowda <116069416+eeranagowda@users.noreply.github.com>
2022-12-27 19:23:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ terraform {
 
 provider "aws" {
         region = "us-east-1" 
-        #profile = "default"
+        profile = "default"
 	}
 
 resource "aws_instance" "Pratiinfra" {
 	ami = "ami-0b5eea76982371e91" 
-	instance_type = "t2.micro"
+	instance_type = "t2.nano"
 }
